50b987ff205abdcf5abd3557fd70e289b4cdb0f2
VinhLin <linducvinh@gmail.com>
2024-06-03 17:13:20 +0700

    feat(module): module read_gps

--- gps_stm32f3/src/task/debug_uart.rs
+++ gps_stm32f3/src/task/debug_uart.rs
@@ -1,41 +1,50 @@
-#[warn(unused_imports)]
+// #[warn(unused_imports)]
 use core::fmt::Write;
 
 use defmt::*;
-use embassy_stm32::usart::{Config, Uart};
+use defmt_rtt as _;
+
+use embassy_stm32::usart::{Config, Uart, UartTx};
 use embassy_stm32::{bind_interrupts, peripherals, usart};
 use heapless::String;
-use {defmt_rtt as _, panic_probe as _};
+// use {defmt_rtt as _, panic_probe as _};
 // use panic_halt as _;
 // use embassy_executor::task;
+// use embassy_stm32::dma::NoDma;
 
 bind_interrupts!(struct Irqs {
     USART1 => usart::InterruptHandler<peripherals::USART1>;
 });
 
-#[embassy_executor::task]
-pub async fn write_command(text: &'static str) {
+// #[embassy_executor::task]
+pub async fn show_data_debug(message: &str) {
     let p = embassy_stm32::init(Default::default());
     info!("Write Command");
 
-    // bind_interrupts!(struct Irqs {
-    //     USART1 => usart::InterruptHandler<peripherals::USART1>;
-    // });
+    let mut config_debug = Config::default();
+    config_debug.baudrate = 115200;
 
-    let config = Config::default();
-    let mut usart = Uart::new(
-        p.USART1, p.PA10, p.PA9, Irqs, p.DMA1_CH4, p.DMA1_CH5, config,
+    // let mut usart_debug_tx = UartTx::new(p.USART1, p.PA9, NoDma, config_debug).unwrap();
+
+    let mut usart_debug_tx = Uart::new(
+        p.USART1,
+        p.PA10,
+        p.PA9,
+        Irqs,
+        p.DMA1_CH4,
+        p.DMA1_CH5,
+        config_debug,
     )
     .unwrap();
 
-    let mut s: String<128> = String::new();
-    core::write!(&mut s, "{}\r\n", text).unwrap();
+    // let mut s: String<128> = String::new();
+    // core::write!(&mut s, "{}\r\n", message).unwrap();
 
-    let _ = usart.write(s.as_bytes()).await;
+    // let _ = usart_debug_tx.write(s.as_bytes()).await;
+    // usart_debug_tx.write(s.as_bytes()).await.unwrap();
 
-    // loop {
-    //     let _ = usart.write(b"OK\r\n").await;
-    // }
+    // usart_debug_tx.write(message.as_bytes()).await.unwrap();
+    usart_debug_tx.blocking_write(message.as_bytes()).unwrap();
 }
 
 // #[embassy_executor::task]
@@ -56,8 +65,8 @@ pub async fn write_command(text: &'static str) {
 //     // Attempt to convert the byte array to a &str
 //     let text_str = match core::str::from_utf8(text) {
 //         Ok(s) => s,
-//         Err(e) => {
-//             info!("Failed to convert byte array to &str: {}", e);
+//         Err(_e) => {
+//             // info!("Failed to convert byte array to &str: {}", e);
 //             return;
 //         }
 //     };
